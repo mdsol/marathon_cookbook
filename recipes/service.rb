bcfca03f1bc0d778c9f1fb3cd0d93daa1c0eb12d
Nathan Gourley <ngourley@gmail.com>
2015-10-26 14:35:46 -0500

    Add case statements for init systems.

--- recipes/service.rb
+++ recipes/service.rb
@@ -4,7 +4,27 @@
 #
 
 template 'marathon-init' do
-  path   '/etc/init/marathon.conf'
-  source 'upstart.erb'
+  case node['marathon']['init']
+  when 'systemd'
+    path '/usr/lib/systemd/system/mesos-slave.service'
+    source 'systemd.erb'
+  when 'upstart'
+    path   '/etc/init/marathon.conf'
+    source 'upstart.erb'
+  when 'sysvinit_debian'
+    path '/etc/init.d/mesos-slave'
+    source 'sysvinit_debian.erb'
   variables(wrapper: ::File.join(node['marathon']['home'], 'wrapper'))
 end
+
+# service 'marathon-default' do
+#   case node['marathon']['init']
+#   when 'systemd'
+#     provider Chef::Provider::Service::Systemd
+#   when 'sysvinit_debian'
+#     provider Chef::Provider::Service::Init::Debian
+#   when 'upstart'
+#     provider Chef::Provider::Service::Upstart
+#   end
+#   action [:stop, :disable]
+# end

1dd274f78aba08986adf04b7a38b925392bbcf1a
Ramez Hanna <rhanna@informatiq.org>
2015-11-13 17:06:29 +0100

    use version attribute in URL

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ default['java']['jdk_version']            = '8'
 
 # Marathon package
 default['marathon']['version']            = '0.11.0'
-default['marathon']['source']['url']      = 'http://downloads.mesosphere.com/marathon/v0.11.0/marathon-0.11.0.tgz'
+default['marathon']['source']['url']      = "http://downloads.mesosphere.com/marathon/v#{node['marathon']['version']}/marathon-#{node['marathon']['version']}.tgz"
 default['marathon']['source']['checksum'] = 'e6c9de73e6fb9ce7b3ff53ab706bbd427f4479dfce4172c983ff13f7330f600e'
 
 # Marathon user and directories

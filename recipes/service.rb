8725807b18b04c5ba0926a116409498302335208
Ramez Hanna <r.hanna@criteo.com>
2015-11-24 10:37:45 +0100

    Always run marathon as the node['marathon']['user']

--- recipes/service.rb
+++ recipes/service.rb
@@ -6,7 +6,8 @@
 template 'marathon-init' do
   path   '/etc/init/marathon.conf'
   source 'upstart.erb'
-  variables(wrapper: ::File.join(node['marathon']['home'], 'wrapper'))
+  variables(wrapper: ::File.join(node['marathon']['home'], 'wrapper'),
+            user:    node['marathon']['user'])
 end
 
 service 'marathon' do

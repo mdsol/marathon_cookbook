c94bbf2a2191e7ee2abf610daf690307b5c2919d
Jeremy MAURO <j.mauro@criteo.com>
2016-01-11 23:32:28 +0100

    Updating test cookbook to also use poise

--- test/cookbooks/zookeeper-service/recipes/default.rb
+++ test/cookbooks/zookeeper-service/recipes/default.rb
@@ -7,18 +7,7 @@ include_recipe 'zookeeper'
 
 zk_path = ::File.join(node['zookeeper']['install_dir'], "zookeeper-#{node['zookeeper']['version']}")
 
-# Configure the Zookeeper service
-template 'zookeeper-upstart' do
-  path     '/etc/init/zookeeper.conf'
-  source   'zookeeper.conf.erb'
-  variables(bin: ::File.join(zk_path, 'bin', 'zkServer.sh'))
-  action   :create
-  notifies :stop,  'service[zookeeper]', :immediately
-  notifies :start, 'service[zookeeper]', :immediately
-end
-
-service 'zookeeper' do
-  provider   Chef::Provider::Service::Upstart
-  action     [:enable, :start]
-  subscribes :restart, "zookeeper_config[#{zk_path}/conf/zoo.cfg]", :delayed
+poise_service 'zookeeper' do
+  command "#{::File.join(zk_path, 'bin', 'zkServer.sh')} start-foreground"
+  options 'upstart', template: 'zookeeper.conf.erb'
 end

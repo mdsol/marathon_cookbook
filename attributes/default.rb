e103dff41b037effb49bc9c0d3c236ad1224bc79
Robert Veznaver <r.veznaver@criteo.com>
2016-02-22 17:20:04 +0100

    Update RuboCop rules

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,8 @@ default['java']['jdk_version']            = '8'
 
 # Marathon package
 default['marathon']['version']            = '0.13.0'
-default['marathon']['source']['url']      = "http://downloads.mesosphere.com/marathon/v#{node['marathon']['version']}/marathon-#{node['marathon']['version']}.tgz"
+default['marathon']['source']['url']      =
+  "http://downloads.mesosphere.com/marathon/v#{node['marathon']['version']}/marathon-#{node['marathon']['version']}.tgz"
 default['marathon']['source']['checksum'] = '224154d19679d70fb2e8e6e90c389c9542d1ece0af38be16a134e96ec587e130'
 default['marathon']['syslog']             = true
 
